4d61cf8545602bf06dbf4e28f343f57c9c588d04
reactVishnu <thirdparties47@gmail.com>
2023-07-22 15:42:50 +0530

    Postgresql Changes

---
+++ app/app/test.py
@@ -0,0 +1,16 @@
+# Sample Test
+
+from django.test import SimpleTestCase
+
+from . import calc
+
+
+class CalcTest(SimpleTestCase):
+
+    def test_add_numbers(self):
+        res = calc.add(5, 6)
+        self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        res = calc.subtract(6, 5)
+        self.assertEqual(res, 1)
