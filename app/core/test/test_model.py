05000f40c8e8a203cc12499a4e4843dea26a0dd3
reactVish <thirdparties47@gmail.com>
2023-07-24 14:27:55 +0530

    Sync Changes

---
+++ app/core/test/test_model.py
@@ -0,0 +1,32 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTest(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        email = 'test@example.com'
+        password = 'testpass123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password,
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+
+    def test_new_user_email_normalized(self):
+
+        sample_emails = [['test1@EXAMPLE.com', 'test1@example.com'],
+                         ['TEST2@EXAMPLE.COM', 'TEST2@example.com'],
+                         ['TEST3@EXample.com', 'TEST3@example.com']
+                         ]
+
+        for email , expected in sample_emails:
+            user = get_user_model().objects.create_user(email, 'sample123')
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('','test123')
