29fcfb99c1cdb820e399ff900bfaa952f5153713
reactVish <thirdparties47@gmail.com>
2023-07-25 19:02:59 +0530

    Last Try Bruh

--- app/core/models.py
+++ app/core/models.py
@@ -1,28 +1,36 @@
 from django.db import models
 
 # Create your models here.
-from django.contrib.auth.models import (AbstractUser, BaseUserManager, PermissionsMixin)
+from django.db import models
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager, PermissionsMixin)
 
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
-        username = email
         if not email:
             raise ValueError('User must have an email address')
-        user = self.model(email=self.normalize_email(email),username = username, **extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
         return user
 
 
-class User(AbstractUser, PermissionsMixin):
-    '''User in the system.'''
+class User(AbstractBaseUser, PermissionsMixin):
+
+    '''User in the system'''
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
-    is_active = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
-
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
